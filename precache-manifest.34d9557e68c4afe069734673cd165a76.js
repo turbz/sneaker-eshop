5ad503cf5258cff71035994f06351184d18c8de3
thabisoddx <thabisomoha@gmail.com>
2020-04-20 11:53:15 +0200

    Updates

---
+++ precache-manifest.34d9557e68c4afe069734673cd165a76.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ea4afd3b73f21d5c356b0124a66cc4",
+    "url": "/sneaker-eshop/index.html"
+  },
+  {
+    "revision": "bf0be43bb41124cdf86e",
+    "url": "/sneaker-eshop/static/css/2.ab8a0683.chunk.css"
+  },
+  {
+    "revision": "8592a001524266b617ca",
+    "url": "/sneaker-eshop/static/css/main.135e2f1b.chunk.css"
+  },
+  {
+    "revision": "bf0be43bb41124cdf86e",
+    "url": "/sneaker-eshop/static/js/2.878b8eed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sneaker-eshop/static/js/2.878b8eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8592a001524266b617ca",
+    "url": "/sneaker-eshop/static/js/main.7ff298e3.chunk.js"
+  },
+  {
+    "revision": "36258269848e55c1fe8f",
+    "url": "/sneaker-eshop/static/js/runtime-main.de79b4c7.js"
+  },
+  {
+    "revision": "9991d4a42b3934df29256568b98cc096",
+    "url": "/sneaker-eshop/static/media/arrow-left.9991d4a4.svg"
+  },
+  {
+    "revision": "965444ae36bfefbc7541e957609f7b8c",
+    "url": "/sneaker-eshop/static/media/bag.965444ae.svg"
+  },
+  {
+    "revision": "3b6eeabf897e6ecebd560204f3d02034",
+    "url": "/sneaker-eshop/static/media/bg-header.3b6eeabf.svg"
+  },
+  {
+    "revision": "7b943269dbc765009b0471f578fa6ad8",
+    "url": "/sneaker-eshop/static/media/close-icon.7b943269.svg"
+  },
+  {
+    "revision": "49dbfe7005cbf19f2fab649b43979e2d",
+    "url": "/sneaker-eshop/static/media/facebook.49dbfe70.svg"
+  },
+  {
+    "revision": "91166c5d99cd74e4294651faf0be83b0",
+    "url": "/sneaker-eshop/static/media/instagram.91166c5d.svg"
+  },
+  {
+    "revision": "87f8c2050c88e2cbde28bb4d37da3f97",
+    "url": "/sneaker-eshop/static/media/linkedin.87f8c205.svg"
+  },
+  {
+    "revision": "42a72b415022751c51fe3ab8096c8bfd",
+    "url": "/sneaker-eshop/static/media/logo.42a72b41.png"
+  },
+  {
+    "revision": "905426b4a41e8f38f83b6fcbdb1dbb7c",
+    "url": "/sneaker-eshop/static/media/menu-icon.905426b4.svg"
+  },
+  {
+    "revision": "756edaa4445041ca6f035c0c1165b356",
+    "url": "/sneaker-eshop/static/media/remove-shopping-cart.756edaa4.svg"
+  },
+  {
+    "revision": "2ceb361ce78c39f8bd1533cd6a379b82",
+    "url": "/sneaker-eshop/static/media/search-icon.2ceb361c.svg"
+  },
+  {
+    "revision": "a4ab47696fa21ecf9b584a32587e5e39",
+    "url": "/sneaker-eshop/static/media/shopping-cart.a4ab4769.svg"
+  },
+  {
+    "revision": "a7182df5b5e5acbf68914da296baf2fa",
+    "url": "/sneaker-eshop/static/media/skype.a7182df5.svg"
+  },
+  {
+    "revision": "75944059623ccb98bd4fdef13b52e54b",
+    "url": "/sneaker-eshop/static/media/sneaker1.75944059.png"
+  },
+  {
+    "revision": "9410284f3cee47de1d0bcb795d81ca2e",
+    "url": "/sneaker-eshop/static/media/sneaker2.9410284f.png"
+  },
+  {
+    "revision": "83e9c874e674cbbffa690481f2dd049a",
+    "url": "/sneaker-eshop/static/media/sneaker3.83e9c874.png"
+  },
+  {
+    "revision": "c627ec2b3b50cfb41c846324ca054ddf",
+    "url": "/sneaker-eshop/static/media/sneaker4.c627ec2b.png"
+  },
+  {
+    "revision": "17db471ab7249ea743df45c8f87cebba",
+    "url": "/sneaker-eshop/static/media/twitter.17db471a.svg"
+  },
+  {
+    "revision": "7f16fd8c320966866d04e6738d060591",
+    "url": "/sneaker-eshop/static/media/youtube.7f16fd8c.svg"
+  }
+]);
